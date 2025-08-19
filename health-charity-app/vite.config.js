1f936584b32d58d3de2572bade325621eae711ef
Larry <100991796+SuperFrenkie@users.noreply.github.com>
2025-08-19 18:14:07 +0800

    Update vite.config.js

--- health-charity-app/vite.config.js
+++ health-charity-app/vite.config.js
@@ -15,5 +15,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
-  base: process.env.NODE_ENV === 'production' ? '/FIT5032_2025_Yikang_Chen_35523182_/health-charity-app/' : '/',
+  base: '/',
 })
